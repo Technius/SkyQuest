8b942b8e36aa9226fafb6ac3ae0efdbb73907ab3
Technius <bryan99tan@gmail.com>
2012-02-02 20:44:54 -0800

    Added files

---
+++ src/net/skycraftmc/SkyQuest/SkyQuestMain.java
@@ -0,0 +1,15 @@
+package net.skycraftmc.SkyQuest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkyQuestMain extends JavaPlugin
+{
+	public void onEnable() 
+	{
+		
+	}
+	public void onDisable() 
+	{
+		
+	}
+}
