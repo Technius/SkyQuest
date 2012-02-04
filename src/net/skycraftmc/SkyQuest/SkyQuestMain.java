bfb9d811c72583c23f784ee7132668204d4de467
Technius <bryan99tan@gmail.com>
2012-02-03 18:50:24 -0800

    Added the stuff

--- src/net/skycraftmc/SkyQuest/SkyQuestMain.java
+++ src/net/skycraftmc/SkyQuest/SkyQuestMain.java
@@ -1,12 +1,15 @@
 package net.skycraftmc.SkyQuest;
 
+import net.skycraftmc.SkyQuest.quest.QuestManager;
+
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public class SkyQuestMain extends JavaPlugin
 {
 	Logger log = this.getLogger();
-	ConfigManager cm = new ConfigManager(this);
+	public ConfigManager cm = new ConfigManager(this);
+	public QuestManager qm = new QuestManager(this);
 	public void onEnable() 
 	{
 		if(!cm.checkFiles())cm.createFiles(false);
