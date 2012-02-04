ac3f902d934562a164185a3662bf07fcb03cc81c
Technius <bryan99tan@gmail.com>
2012-02-03 18:50:11 -0800

    Added saving/loading things, QuestManager for commands later

--- src/net/skycraftmc/SkyQuest/ConfigManager.java
+++ src/net/skycraftmc/SkyQuest/ConfigManager.java
@@ -1,10 +1,15 @@
 package net.skycraftmc.SkyQuest;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConfigManager 
 {
@@ -17,6 +22,8 @@ public class ConfigManager
 	{
 		File dataFolder = new File(main.getDataFolder().getPath());
 		if(!dataFolder.exists())dataFolder.mkdir();
+		File quests = new File(dataFolder.getPath() + File.separator + "Quests");
+		if(!quests.exists())quests.mkdir();
 		File config = new File(dataFolder.getPath() + File.separator + "config.txt");
 		try
 		{
@@ -37,12 +44,61 @@ public class ConfigManager
 			main.log.severe("COULD NOT CREATE CONFIG! Expect errors.");
 		}
 	}
+	public void loadFiles()
+	{
+		File dataFolder = new File(main.getDataFolder().getPath());
+		File quests = new File(dataFolder.getPath() + File.separator + "Quests");
+		File config = new File(dataFolder.getPath() + File.separator + "config.txt");
+		try
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(config)));
+			String l;
+			while((l=br.readLine()) != null)
+			{
+				if(l.startsWith("#"))continue;
+			}
+			br.close();
+			for(File f:quests.listFiles())
+			{
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+				int objective = 1;
+				String name = null;
+				List<String> rewards = new ArrayList<String>();
+				String text = null;
+				while((l=br.readLine()) != null)
+				{
+					if(l.startsWith("#"))continue;
+					String[] tokens = l.split("[:]", 2);
+					if(tokens.length != 2)continue;
+					if(tokens[0].startsWith("objective"))
+					{
+						String[] tok = tokens[0].split("[ ]", 2);
+						if(tok.length == 2)
+						{
+							try{objective = Integer.parseInt(tok[1]);}catch(NumberFormatException nfe){}
+						}
+						name = tokens[1].trim();
+					}
+					if(name != null && text != null)
+					{
+						
+					}
+				}
+			}
+		}
+		catch(IOException ex)
+		{
+			main.log.severe("COULD NOT LOAD FILES! Expect errors.");
+		}
+	}
 	public boolean checkFiles()
 	{
 		File dataFolder = new File(main.getDataFolder().getPath());
 		if(!dataFolder.exists())return false;
 		File config = new File(dataFolder.getPath() + File.separator + "config.txt");
 		if(!config.exists())return false;
+		File quests = new File(dataFolder.getPath() + File.separator + "Quests");
+		if(!quests.exists())return false;
 		return true;
 	}
 }
---
+++ src/net/skycraftmc/SkyQuest/quest/QuestManager.java
@@ -0,0 +1,30 @@
+package net.skycraftmc.SkyQuest.quest;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+import net.skycraftmc.SkyQuest.SkyQuestMain;
+
+public class QuestManager 
+{
+	private SkyQuestMain p;
+	public static HashMap<Player, List<Quest>> quests = new HashMap<Player, List<Quest>>();
+	public QuestManager(SkyQuestMain main)
+	{
+		p = main;
+	}
+	public void addQuest(Player player, Quest quest)
+	{
+		quests.get(player).add(quest);
+	}
+	public List<Quest> getQuests(Player player)
+	{
+		return quests.get(player);
+	}
+	public Quest getQuest(String quest)
+	{
+		return null;
+	}
+}
