baf49ac4bd3aeb2bc746d5f34597842080ffdc44
Technius <bryan99tan@gmail.com>
2012-02-07 16:31:59 -0800

    Register listeners

--- src/net/skycraftmc/SkyQuest/SkyQuestMain.java
+++ src/net/skycraftmc/SkyQuest/SkyQuestMain.java
@@ -8,11 +8,13 @@ import java.util.logging.Logger;
 public class SkyQuestMain extends JavaPlugin
 {
 	Logger log = this.getLogger();
+	public SkyQuestListener l = new SkyQuestListener(this);
 	public ConfigManager cm = new ConfigManager(this);
 	public QuestManager qm = new QuestManager(this);
 	public void onEnable() 
 	{
 		if(!cm.checkFiles())cm.createFiles(false);
+		this.getServer().getPluginManager().registerEvents(l, this);
 		log.info("Version " + getDescription().getVersion() + " enabled!");
 	}
 	public void onDisable() 
