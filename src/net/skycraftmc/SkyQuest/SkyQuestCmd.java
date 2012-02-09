1d051f96fc34081ac59c8f1f31072f3c653f1a8e
Technius <bryan99tan@gmail.com>
2012-02-08 17:19:38 -0800

    Added more commands

--- src/net/skycraftmc/SkyQuest/SkyQuestCmd.java
+++ src/net/skycraftmc/SkyQuest/SkyQuestCmd.java
@@ -1,6 +1,10 @@
 package net.skycraftmc.SkyQuest;
 
+import java.util.ArrayList;
+
+import net.skycraftmc.SkyQuest.quest.Quest;
 import net.skycraftmc.SkyQuest.quest.QuestManager;
+import net.skycraftmc.SkyQuest.util.SkyQuestUtil;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -56,9 +60,53 @@ public class SkyQuestCmd implements CommandExecutor
 						}
 						if(plugin.qm.getQuest(s) != null)
 						{
-							
+							plugin.qm.addQuest(target, plugin.qm.getQuest(s));
+							sender.sendMessage(ChatColor.GREEN + s + " has been given to " + target.getName());
 						}
 						else sender.sendMessage(ChatColor.RED + "No such quest: " + s);
+					}
+				}
+				else menu(sender, 1);
+			}
+			else if(args.length >= 1)
+			{
+				if(args[0].equalsIgnoreCase("info"))
+				{
+					if(player == null)sender.sendMessage("You cannot use this command!");
+					else
+					{
+						String[] na = SkyQuestUtil.argsWithQuotes(args);
+						if(na.length != 2)menu(player, 1);
+						else
+						{
+							Quest q = plugin.qm.getQuest(na[1]);
+							if(q != null)
+							{
+								if(!plugin.qm.hasQuest(player, q.getName()))
+								{
+									player.sendMessage(ChatColor.RED + "You don't have this quest");
+									return true;
+								}
+								Quest pq = plugin.qm.getQuest(player, q.getTitle());
+								player.sendMessage(ChatColor.GOLD + "|===( Quest Info: " + ChatColor.RED + pq.getTitle() + " )===|");
+								player.sendMessage(ChatColor.AQUA + "Objective: " + ChatColor.GREEN + pq.getCurrentObjective().getLabel());
+								player.sendMessage(ChatColor.AQUA + "Description: " + ChatColor.GREEN + pq.getCurrentObjective().getText());
+							}
+							else player.sendMessage(ChatColor.RED + "No such quest: " + na[1]);
+						}
+					}
+				}
+				else if(args[0].equalsIgnoreCase("list"))
+				{
+					if(player == null)sender.sendMessage("You cannot use this command!");
+					else
+					{
+						ArrayList<Quest> q = (ArrayList<Quest>) plugin.qm.getQuests(player);
+						player.sendMessage(ChatColor.GOLD + "|=====(Quest List)=====|");
+						for(Quest qx: q)
+						{
+							player.sendMessage(ChatColor.RED + qx.getTitle());
+						}
 					}
 				}
 				else menu(sender, 1);
