234cc3de7494bfd08993a8bd1f1e184e5760b87a
Technius <bryan99tan@gmail.com>
2012-02-08 17:00:56 -0800

    Added argsWithQuotes

--- src/net/skycraftmc/SkyQuest/util/SkyQuestUtil.java
+++ src/net/skycraftmc/SkyQuest/util/SkyQuestUtil.java
@@ -1,5 +1,7 @@
 package net.skycraftmc.SkyQuest.util;
 
+import java.util.ArrayList;
+
 import org.bukkit.entity.CreatureType;
 
 public class SkyQuestUtil 
@@ -16,5 +18,23 @@ public class SkyQuestUtil
 			if(s.toUpperCase().replaceAll("_", " ").equalsIgnoreCase("ZOMBIE PIGMAN"))return CreatureType.PIG_ZOMBIE;
 		}
 		return type;
+	}
+	public static String[] argsWithQuotes(String[] args)
+	{
+		String s = "";
+		boolean in = false;
+		ArrayList<String> sa = new ArrayList<String>();
+		for(String x:args)
+		{
+			if(x.startsWith("\""))in = true;
+			s = s + x;
+			if(x.endsWith("\""))
+			{
+				in = false;
+				sa.add(s.replaceAll("\"", ""));
+			}
+			if(!in)sa.add(x);
+		}
+		return sa.toArray(new String[sa.size()]);
 	}
 }
