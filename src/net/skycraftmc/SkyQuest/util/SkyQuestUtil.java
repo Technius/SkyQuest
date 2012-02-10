d0536cb416988417cf491b3c897b3a64df30d4da
Technius <bryan99tan@gmail.com>
2012-02-09 20:55:43 -0800

    Added another getType

--- src/net/skycraftmc/SkyQuest/util/SkyQuestUtil.java
+++ src/net/skycraftmc/SkyQuest/util/SkyQuestUtil.java
@@ -2,7 +2,16 @@ package net.skycraftmc.SkyQuest.util;
 
 import java.util.ArrayList;
 
+import org.bukkit.entity.Blaze;
+import org.bukkit.entity.CaveSpider;
 import org.bukkit.entity.CreatureType;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Enderman;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Spider;
+import org.bukkit.entity.Zombie;
 
 public class SkyQuestUtil 
 {
@@ -11,13 +20,25 @@ public class SkyQuestUtil
 		CreatureType type = null;
 		try
 		{
-			type = CreatureType.valueOf(s.replaceAll(" ", "_"));
+			type = CreatureType.valueOf(s.replaceAll(" ", "_").toUpperCase());
 		}
 		catch(IllegalArgumentException iae)
 		{
 			if(s.toUpperCase().replaceAll("_", " ").equalsIgnoreCase("ZOMBIE PIGMAN"))return CreatureType.PIG_ZOMBIE;
 		}
 		return type;
+	}
+	public static String getType(Entity e)
+	{
+		if(e instanceof Creeper)return "Creeper";
+		if(e instanceof Player)return "Player";
+		if(e instanceof Zombie)return "Zombie";
+		if(e instanceof Skeleton)return "Skeleton";
+		if(e instanceof Enderman)return "Enderman";
+		if(e instanceof Spider)return "Spider";
+		if(e instanceof CaveSpider)return "CaveSpider";
+		if(e instanceof Blaze)return "Blaze";
+		return null;
 	}
 	public static String[] argsWithQuotes(String[] args)
 	{
@@ -27,11 +48,12 @@ public class SkyQuestUtil
 		for(String x:args)
 		{
 			if(x.startsWith("\""))in = true;
-			s = s + x;
+			if(in)s = s + x;
 			if(x.endsWith("\""))
 			{
 				in = false;
 				sa.add(s.replaceAll("\"", ""));
+				s = "";
 			}
 			if(!in)sa.add(x);
 		}
