b9b3d4ccfb8022036a319d8d64c0e721caea43b7
Technius <bryan99tan@gmail.com>
2012-04-30 21:35:50 -0700

    Addition of some files

--- src/net/skycraftmc/SkyQuest/SkyQuestMain.java
+++ src/net/skycraftmc/SkyQuest/SkyQuestMain.java
@@ -1,5 +1,7 @@
 package net.skycraftmc.SkyQuest;
 
+import net.skycraftmc.SkyQuest.quest.QuestManager;
+
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
@@ -8,16 +10,21 @@ public class SkyQuestMain extends JavaPlugin
 	Logger log;
 	//public SkyQuestListener l = new SkyQuestListener(this);
 	//public ConfigManager cm = new ConfigManager(this);
-	//public QuestManager qm = new QuestManager(this);
+	private static QuestManager qm = null;
 	public SkyQuestCmd command = new SkyQuestCmd(this);
 	public void onEnable() 
 	{
 		log = this.getLogger();
+		qm = new QuestManager(this);
 		getCommand("quest").setExecutor(command);
 		log.info("Version " + getDescription().getVersion() + " enabled!");
 	}
 	public void onDisable() 
 	{
 		log.info("Version " + getDescription().getVersion() + " disabled!");
+	}
+	public static QuestManager getQuestManager()
+	{
+		return qm;
 	}
 }
---
+++ src/net/skycraftmc/SkyQuest/quest/QuestManager.java
@@ -0,0 +1,12 @@
+package net.skycraftmc.SkyQuest.quest;
+
+import net.skycraftmc.SkyQuest.SkyQuestMain;
+
+public class QuestManager 
+{
+	private SkyQuestMain plugin;
+	public QuestManager(SkyQuestMain plugin)
+	{
+		this.plugin = plugin;
+	}
+}
