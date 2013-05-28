4bb080dcd1da8c0c1660c265b2a87e01a2928fa2
Technius <techniux@gmail.com>
2013-05-27 17:30:27 -0700

    Use 1.5.2 CB

--- src/net/skycraftmc/SkyQuest/util/BukkitUtil.java
+++ src/net/skycraftmc/SkyQuest/util/BukkitUtil.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
-import net.minecraft.server.v1_5_R2.NBTBase;
+import net.minecraft.server.v1_5_R3.NBTBase;
 import net.skycraftmc.SkyQuest.util.nbt.TagBase;
 
 public class BukkitUtil
