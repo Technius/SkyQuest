e425da847a021fec0f49fcedbf898859bb130396
Technius <techniux@gmail.com>
2013-06-08 16:41:20 -0700

    Translate color codes for MessageAction

--- src/net/skycraftmc/SkyQuest/action/MessageAction.java
+++ src/net/skycraftmc/SkyQuest/action/MessageAction.java
@@ -2,6 +2,7 @@ package net.skycraftmc.SkyQuest.action;
 
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class MessageAction extends ActionType
@@ -12,7 +13,7 @@ public class MessageAction extends ActionType
 			throw new IllegalArgumentException("action is not valid");
 		Player p = Bukkit.getServer().getPlayerExact(player);
 		if(p == null)return false;
-		p.sendMessage(action);
+		p.sendMessage(ChatColor.translateAlternateColorCodes('&', action));
 		return true;
 	}
 
